99bf309a1a771cd38781142625da5db6df0115a6
huseynesedov <asadof28@gmail.com>
2023-09-26 20:47:35 +0400

    new

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FaGithub } from 'react-icons/fa';
+
+const HeaderSocials = () => {
+  return (
+    <div className="header__socials">
+      <a href="https://github.com/huseynesedov" target="_blank" rel="noreferrer" ><FaGithub /></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,55 @@
+import "./portfolio.css";
+
+import IMG1 from "../../assets/Electric-scooter.PNG";
+
+import React from "react";
+
+//Portfolio function
+const Portfolio = () => {
+  const soloProjects = [
+    {
+      id: 1,
+      title: "Electric Scooter",
+      img: IMG1,
+      description:
+        "demo web site for people to buy scooters and scooter locks conveniently.",
+      technologies: "React | CSS | Redux ",
+      link: "https://educational-website-reactjs.netlify.app/",
+    }
+  ];
+
+  return (
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        {soloProjects.map((pro) => (
+          <article className="portfolio__item" key={pro.id}>
+            <div className="portfolio__item-image">
+              <img src={pro.img} alt={pro.title} />
+            </div>
+            <div className="portfolio__item-content">
+              <h3>{pro.title}</h3>
+              <p>{pro.description}</p>
+              <p>{pro.technologies}</p>
+            </div>
+            <div className="portfolio__item-cta">
+              
+              <a
+                href={pro.link}
+                target="_blank"
+                className="btn btn-primary"
+                rel="noreferrer"
+              >
+                Live Demo
+              </a>
+            </div>
+          </article>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
