f36bdf1313a7c8ad64515c1ae16c36a75c876a0a
huseynesedov <asadof28@gmail.com>
2023-10-04 23:59:14 +0400

    new:url

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,7 +6,7 @@ import './footer.css';
 const Footer = () => {
   return (
     <footer>
-      <a href="#home" className="footer__logo">Huseyn Esedov</a>
+      <a href="#home" className="footer__logo">HUSSEIN ASADOV</a>
       <ul className="permalinks">
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
