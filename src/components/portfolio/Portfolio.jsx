25010730e56538208b26cf39337ff48c55904804
huseynesedov <asadof28@gmail.com>
2024-01-29 17:36:41 +0400

    ..

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,6 +3,8 @@ import "./portfolio.css";
 import IMG1 from "../../assets/Electric-scooter.PNG";
 import IMG2 from "../../assets/sudoku.PNG";
 import IMG3 from "../../assets/dragdrop.PNG";
+import IMG4 from "../../assets/Education.PNG";
+import IMG5 from "../../assets/SabiMed.PNG ";
 
 import React from "react";
 
@@ -14,12 +16,35 @@ const Portfolio = () => {
       title: "Electric Scooter",
       img: IMG1,
       description:
-        "demo web site for people to buy scooters and scooter locks conveniently.",
+        "Demo website for people to buy scooters and scooter locks conveniently.",
       technologies: "React | CSS | Redux ",
       link: "https://electric-scooter.vercel.app",
     },
     {
       id: 2,
+      title: "SabiMed",
+      img: IMG5,
+      description:
+        "Demo website to provide people with health services conveniently.",
+      technologies: "React | CSS | Redux ",
+      link: "https://sabimed.vercel.app/",
+    },
+    {
+      id: 3,
+      title: "Academia Education",
+      img: IMG4,
+      description:
+        "Demo website for people's convenient education.",
+      technologies: "React | CSS | Redux ",
+      link: "https://education-bay.vercel.app/",
+    }
+
+
+  ];
+  const games = [
+
+    {
+      id: 1,
       title: "Sudoku Game",
       img: IMG2,
       description:
@@ -28,21 +53,31 @@ const Portfolio = () => {
       link: "https://sudokugamev1.vercel.app/",
     },
     {
-      id: 3,
+      id: 2,
       title: "Drag And Drop",
       img: IMG3,
       description:
         "demo web site for people to Drag Game and Drop locks conveniently.",
       technologies: "Html | CSS | Js ",
       link: "https://drag-dropv1.vercel.app/",
+    }, 
+    {
+      id: 3,
+      title: "Todo List",
+      img: IMG3,
+      description:
+        "demo web site for people to Todo List and Todo conveniently.",
+      technologies: "Html | CSS | Js ",
+      link: "https://todolist-tau-neon.vercel.app/",
     }
 
   ];
-
   return (
     <section id="portfolio">
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
+      <br />
+      <h2>Web Sites</h2>
 
       <div className="container portfolio__container">
         {soloProjects.map((pro) => (
@@ -56,9 +91,38 @@ const Portfolio = () => {
               <p>{pro.technologies}</p>
             </div>
             <div className="portfolio__item-cta">
-              
+
               <a
                 href={pro.link}
+                target="_blank"
+                className="btn btn-primary"
+                rel="noreferrer"
+              >
+                Live Demo
+              </a>
+            </div>
+          </article>
+        ))}
+      </div>
+      <br />
+
+      <h2>Web Games</h2>
+
+      <div className="container portfolio__container">
+        {games.map((pros) => (
+          <article className="portfolio__item" key={pro.id}>
+            <div className="portfolio__item-image">
+              <img src={pros.img} alt={pros.title} />
+            </div>
+            <div className="portfolio__item-content">
+              <h3>{pros.title}</h3>
+              <p>{pros.description}</p>
+              <p>{pros.technologies}</p>
+            </div>
+            <div className="portfolio__item-cta">
+
+              <a
+                href={pros.link}
                 target="_blank"
                 className="btn btn-primary"
                 rel="noreferrer"
